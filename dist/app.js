e1dc512a734bc3d95c86627281e2a5c75a2481e9
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-26 00:46:01 +0330

    Feat: Get modal component

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,3 @@
 "use strict";
 const showModalBtns = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,2 @@
 const showModalBtns = document.querySelectorAll(".show-modal") ! as NodeList;
+const modal = document.querySelector(".modal") ! as HTMLDivElement
