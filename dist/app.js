602d29fdffd146a7cd706168852cef511c3910c3
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-26 00:48:38 +0330

    Feat: Selected close modal btn

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,4 @@
 "use strict";
 const showModalBtns = document.querySelectorAll(".show-modal");
 const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".close-modal");
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,5 @@
-const showModalBtns = document.querySelectorAll(".show-modal") ! as NodeList;
-const modal = document.querySelector(".modal") ! as HTMLDivElement
+const showModalBtns = document.querySelectorAll(".show-modal")! as NodeList;
+const modal = document.querySelector(".modal")! as HTMLDivElement;
+const closeModalBtn = document.querySelector(
+  ".close-modal"
+)! as HTMLButtonElement;
