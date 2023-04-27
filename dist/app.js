f387b357faa9a001a698aae0a521589d4efaff9e
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-27 22:21:15 +0330

    Feat: Implemented show model logic

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,11 @@
 const showModalBtns = document.querySelectorAll(".show-modal");
 const modal = document.querySelector(".modal");
 const closeModalBtn = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const showModal = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+for (let i = 0; i < showModalBtns.length; i++) {
+    showModalBtns[i].addEventListener("click", showModal);
+}
--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,14 @@ const modal = document.querySelector(".modal")! as HTMLDivElement;
 const closeModalBtn = document.querySelector(
   ".close-modal"
 )! as HTMLButtonElement;
+const overlay = document.querySelector(".overlay") ! as HTMLDivElement;
+
+const showModal = () => {
+  modal.classList.remove("hidden")
+  overlay.classList.remove("hidden")
+}
+
+for(let i = 0 ; i < showModalBtns.length ; i++) {
+  showModalBtns[i].addEventListener("click" , showModal)
+}
+
