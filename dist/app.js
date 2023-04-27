f01297df08428fbdef891aa31e770ae69b2792fa
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-28 00:02:59 +0330

    Feat: Implemented keydown press for closing the modal

--- dist/app.js
+++ dist/app.js
@@ -19,3 +19,8 @@ for (let i = 0; i < showModalBtns.length; i++) {
 }
 closeModalBtn.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,28 @@ const modal = document.querySelector(".modal")! as HTMLDivElement;
 const closeModalBtn = document.querySelector(
   ".close-modal"
 )! as HTMLButtonElement;
-const overlay = document.querySelector(".overlay") ! as HTMLDivElement;
+const overlay = document.querySelector(".overlay")! as HTMLDivElement;
 
 // show modal
-const showModal = () : void => {
-  modal.classList.remove("hidden")
-  overlay.classList.remove("hidden")
-}
+const showModal = (): void => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 // close Modal
-const closeModal = () : void => {
-  modal.classList.add("hidden")
-  overlay.classList.add("hidden")
-}
+const closeModal = (): void => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 // event listener for buttons
-for(let i = 0 ; i < showModalBtns.length ; i++) {
-  showModalBtns[i].addEventListener("click" , showModal)
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener("click", showModal);
 }
-closeModalBtn.addEventListener("click" , closeModal)
-overlay.addEventListener("click" , closeModal)
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
